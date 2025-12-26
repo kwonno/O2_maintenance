c58a16c4e9659588764482e1e0c397a69de68a6c
kwonno <knoiq180@gmail.com>
2025-12-26 11:47:47 +0900

    Suppress hydration warnings from browser extensions

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  // 브라우저 확장 프로그램이 추가한 속성 경고 무시
+  onDemandEntries: {
+    maxInactiveAge: 25 * 1000,
+    pagesBufferLength: 2,
+  },
 }
 
 module.exports = nextConfig
